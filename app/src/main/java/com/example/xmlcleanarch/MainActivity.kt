95465867b52bd4a9d759b72d70f4b754cd240a41
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-04-18 12:41:22 +0200

    live data

--- app/src/main/java/com/example/xmlcleanarch/MainActivity.kt
+++ app/src/main/java/com/example/xmlcleanarch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.xmlcleanarch
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +12,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-       
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListener()
+    }
+
+    fun setupListener() {
+        binding.btnLiveData.setOnClickListener {
+            navigate("com.example.xmlcleanarch.screens.live_data.LiveDataActivity")
+        }
+        binding.btnRoom.setOnClickListener {
+            navigate("com.example.xmlcleanarch.screens.room.RoomDBActivity")
+        }
+    }
+    fun navigate(className: String) {
+
+        val intent = Intent(this@MainActivity, Class.forName(className))
+        startActivity(intent)
+
     }
 }
--- app/src/main/java/com/example/xmlcleanarch/screens/live_data/WelcomeViewModel.kt
+++ app/src/main/java/com/example/xmlcleanarch/screens/live_data/WelcomeViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.xmlcleanarch.screens.live_data
 
-class WelcomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+    private val _welcomeMessage = MutableLiveData<String>()
+    val welcomeMessage: LiveData<String>
+        get() = _welcomeMessage
+
+    fun generateWelcomeMessage(name: String) {
+        _welcomeMessage.value = "Welcome, $name!"
+    }
 }
